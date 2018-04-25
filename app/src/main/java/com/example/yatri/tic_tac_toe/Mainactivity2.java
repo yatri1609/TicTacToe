7511641520ce36fd00a20d90917a1e73ec7b1977
Yatri <yatripatel.169@gmail.com>
2018-04-25 16:52:02 +0530

    My first app of tic-tac-toe

---
+++ app/src/main/java/com/example/yatri/tic_tac_toe/Mainactivity2.java
@@ -0,0 +1,15 @@
+package com.example.yatri.tic_tac_toe;
+
+/**
+ * Created by yatri on 3/25/2018.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Mainactivity2 {
+}
